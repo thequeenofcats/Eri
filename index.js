d2b85aea96543f9d938272aad47c90560e47a191
PretendScience <32021273+thequeenofcats@users.noreply.github.com>
2022-06-13 00:51:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const picture = document.querySelector(".cat");
 const load = document.querySelector(".load");
 
 function getImage() {
-  const number = Math.floor(Math.random() * 53);
+  const number = 1 + Math.floor(Math.random() * 53);
   if (number < 21) {
-    picture.classList.remove("horizontal");
-    picture.classList.add("vertical");
-  } else {
-    picture.classList.remove("vertical");
-    picture.classList.add("horizontal");
+    picture.className = "vertical";
+  } else if (number > 21) {
+    picture.className = "horizontal";
   }
   picture.src = "./Assets/" + number + ".jpg";
   picture.style.display = "block";
